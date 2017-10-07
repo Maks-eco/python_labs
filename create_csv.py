f593f2051f9b6799779270319e30389ab9f7cd34
Maks-eco <maks.zawulon@gmail.com>
2017-10-07 14:33:51 +0400

    Add random, rewrite graph run

--- create_csv.py
+++ create_csv.py
@@ -1,11 +1,13 @@
 import csv
+import random
 
 nlist = []
 
 writer = csv.writer(open("file.csv", 'w'))
 reader = csv.reader(nlist)
 for x in range(1,400):
-	nlist.append(str(x**2 - 50) + ", " + str(0.001 * x) + ", " + str(x))
+	x_rand = random.randint(1, 400)
+	nlist.append(str(x_rand**2 - 50) + ", " + str(0.001 * x_rand) + ", " + str(x_rand))
 
 for row in reader:
     writer.writerow(row)
